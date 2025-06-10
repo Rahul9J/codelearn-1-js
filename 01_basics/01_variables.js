4312a3e6ad1ce010782c9b35bcb81029ac986a95
Rahul9J <rahuj9@gmail.com>
2025-06-10 05:22:37 +0000

    variables created

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 99
+let accountEmail = "abc@gmail.com"
+var accountPassword = "123"
+accountCity = "surat"
+
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,5 +1,1 @@
 console.log("RAHUL")
-const accountId = 99
-let accountEmail = "abc@gmail.com"
-var accountPassword = "123"
-accountCity = "surat"
